7877cbae0355f051f9ff3d461b1691eff42184b7
meetrious <meetrious@gmail.com>
2021-07-13 16:07:36 +0700

    3

--- src/MZ_to_diag.lean
+++ src/MZ_to_diag.lean
@@ -1,23 +1,32 @@
 import data.matrix.basic
+import algebra.ring.basic
 
 section pid_module
---variables { M N: Type* } [fintype M] [fintype N]
---[decidable_eq M] [decidable_eq N]
 
 -- Statement_2.1 
 theorem MZ_to_diag
-  (M N : ℕ)
-  (C : matrix (fin M) (fin N) ℤ)
-  (P₁: has_zero (fin N))
-  (P₂: has_zero (fin M))
-  (P₃: has_mul (matrix (fin M) (fin N) ℤ))
-  (P₄: has_mul (matrix (fin N) (fin N) ℤ))
-  (K := max M N )
-  (P : (C 0 0) ≠ 0)
-   : ∃ (U : matrix (fin N) (fin N) ℤ),
-    let A: matrix (fin M) (fin N) ℤ := C ⬝ U in -- как правильно описать умножение?
-    ∀ (i:fin M) (j:fin N), (i ≠ j) → A i j = 0 -- А не определено для индексов типа fin K
-    ∀ (i:fin (K-1)) → (A i i) ∣ (A i+1 i+1) -- как это расписать
+  --(M N : ℕ)
+  --(C : matrix (fin M) (fin N) ℤ)
+  --(P₁: has_zero (fin N))
+  --(P₂: has_zero (fin M))
+  --(P₃: has_one (fin N))
+  --(P₄: has_mul (matrix (fin M) (fin N) ℤ))
+  --(P₅: has_mul (matrix (fin N) (fin N) ℤ))
+  --(K := max M N )
+  {m : Type u_1}{n : Type u_2}
+  [fintype m] [fintype n]
+  {c : Type v} [non_unital_semiring c]
+  (C : matrix m n c)
+  (P₁: has_zero m)
+  (P₂: has_zero n)
+  (P₃ : (C 0 0) ≠ 0)
+  (P₄: has_one n)
+  --(P₅: has_mul (matrix m n ℤ))
+  --(P₆: has_mul (matrix n n ℤ))
+  (i:m)
+  (j:n)
+   : ∃ (U : matrix n n c),
+  let A: matrix m n c := (C ⬝ U) in A 0 0 ≠ 0  A 0 1 = 0  -- как правильно описать умножение?
     :=
 begin
   sorry,
